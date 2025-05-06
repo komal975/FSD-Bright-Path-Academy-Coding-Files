94f14e1fa2f52c8b826e3672e982ea8ac1d693f3
Komal Bhise <komalbhise49@gmail.com>
2025-05-06 08:50:12 +0530

    New Character Patterns

---
+++ CharacterPatternAAABBBCCC.java
@@ -0,0 +1,14 @@
+public class CharacterPatternAAABBBCCC {
+    public static void main(String[] args)
+    {
+        for(char i = 'A'; i <= 'C'; i++)
+        {
+            for(char j = 'A'; j <= 'C'; j++)
+            {
+                System.out.print(" " +i+ " ");
+            }
+        System.out.println();
+        }
+
+    }
+}
---
+++ CharacterPatternABCABCABC.java
@@ -0,0 +1,13 @@
+public class CharacterPatternABCABCABC {
+    public static void main(String[] args)
+    {
+        for(char i = 'A'; i <= 'C'; i++)
+        {
+            for(char j = 'A'; j <= 'C'; j++)
+            {
+                System.out.print(" "+j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
