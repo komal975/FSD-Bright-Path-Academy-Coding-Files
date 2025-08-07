437f90c5179b336d0b32375380c59ec34376dcd4
Komal Bhise <komalbhise49@gmail.com>
2025-08-07 19:44:34 +0530

    First Occurance Index of Array
    # Initialize array using Init() function
    # Display array using Display() function
    # Created FirstOccuranceCheck() function for checking first index of occurance of array & display it.

---
+++ FirstOccuranceIndex.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class FirstOccuranceIndex {
+    
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array :");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        Init(arr);
+    }
+
+    public static int[] Init(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("Enter the data of array :");
+            arr[i] = sc.nextInt();
+        }
+        display(arr);
+        FirstOccuranceCheck(arr);
+        return arr;
+    }
+
+    public static void display(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void FirstOccuranceCheck(int[] arr) {
+        int key;
+
+        System.out.println("Enter the key value :");
+        key = sc.nextInt();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(key == arr[i]) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
